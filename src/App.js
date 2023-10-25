037ae31b9b7b8c4d93e2a83c993632a6d5fbfa18
tiva710 <tiva.rocco@yahoo.com>
2023-10-24 20:58:19 -0700

    Frontend Commit for Assignment 6

--- src/App.js
+++ src/App.js
@@ -8,20 +8,8 @@ import EditAssignment from './components/EditAssignment';
 function App() {
   return (
     <div className="App">
-      <h2>Gradebook</h2>
-      <BrowserRouter>
-          <div>
-            <Switch>
-              <Route exact path="/" component={ListAssignment} />
-              <Route path="/gradeAssignment" component={GradeAssignment} />
-              <Route path="/addAssignment" component={AddAssignment} />
-              <Route path="/editAssignment/:id" component={EditAssignment} />
-              <Route path="/editAssignment/:id/delete" component={EditAssignment} />
-              <Route render={ () => <h1>Page not found</h1>} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-    </div>
+    <Login />
+  </div>
   );
 }
 
